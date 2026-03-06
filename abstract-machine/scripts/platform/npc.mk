ef7dd46b870c27d26d144204043e2a5b1c38b285
tracer-ysyx <tracer@ysyx.org>
2026-03-06 15:53:58 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:53:58 up  5:34,  1 user,  load average: 0.51, 0.52, 0.48

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,7 +12,7 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-# NEMUFLAGS += --l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+NEMUFLAGS += --l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 NEMUFLAGS += --b
 NEMUFLAGS += --e $(IMAGE).elf
 
