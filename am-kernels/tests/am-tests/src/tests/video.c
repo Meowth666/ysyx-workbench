ebc2abaac78a19c09e977bb91d9f9c824c564160
tracer-ysyx <tracer@ysyx.org>
2026-03-06 21:26:20 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:26:20 up 11:07,  1 user,  load average: 1.14, 0.77, 0.72

--- am-kernels/tests/am-tests/src/tests/video.c
+++ am-kernels/tests/am-tests/src/tests/video.c
@@ -14,10 +14,11 @@ static uint32_t canvas[N][N];
 static int used[N][N];
 
 static uint32_t color_buf[32 * 32]; //存储每个区域内的颜色信息
-
+//将整个屏幕划分为若干32×32的块
 void redraw() {
   int w = io_read(AM_GPU_CONFIG).width / N;
   int h = io_read(AM_GPU_CONFIG).height / N;
+  printf("w=%d   h=%d\n",w, h);
   int block_size = w * h;
   assert((uint32_t)block_size <= LENGTH(color_buf));
   int x, y, k;
