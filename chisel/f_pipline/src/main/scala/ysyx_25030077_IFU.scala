79561e938edeab147d71b1b4d35010db975222a3
tracer-ysyx <tracer@ysyx.org>
2026-03-22 20:04:47 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:04:47 up  6:25,  1 user,  load average: 1.32, 1.39, 0.91

--- chisel/f_pipline/src/main/scala/ysyx_25030077_IFU.scala
+++ chisel/f_pipline/src/main/scala/ysyx_25030077_IFU.scala
@@ -18,14 +18,13 @@ class ysyx_25030077_IFU extends Module {
         val r_ready = Output(Bool())
     })
     ChiselHelpers.dontTouchBundleRecursive(io)
-    val sram_tag  = RegInit(VecInit(Seq.fill(16)(0.U(22.W))))
+    val sram_tag  = RegInit(VecInit(Seq.fill(4)(0.U(22.W))))
     val sram_inst = RegInit(VecInit(Seq.fill(16)(0.U(32.W))))
     val sdram_data = RegInit(VecInit(Seq.fill(4)(0.U(32.W))))
-    val sram_valid =  RegInit(VecInit(Seq.fill(16)(0.U(1.W))))
+    val sram_valid =  RegInit(VecInit(Seq.fill(4)(0.U(1.W))))
     val tag = io.pc(27, 6)
     val index = io.pc(5, 4)
     val offset = io.pc(3, 2)
-    val j = RegInit(0.U(2.W))
     val k = RegInit(0.U(2.W))
     val q = RegInit(0.U(2.W))
     val rdata_reg = RegInit(0.U(32.W))
@@ -33,7 +32,7 @@ class ysyx_25030077_IFU extends Module {
     val ar_addr_reg  = RegInit(0.U(32.W))
     io.r_ready := true.B
     val state_reg = RegInit("b00001".U(5.W))
-    val is_hit = ((sram_tag(Cat(index, offset)) === tag) && (sram_valid(Cat(index, offset)) === "b1".U))
+    val is_hit = ((sram_tag(index) === tag) && (sram_valid(index) === "b1".U))
     state_reg := MuxCase("b00001".U(5.W), Seq(
         (state_reg === "b00001".U(5.W)) -> Mux(io.pc(31, 28) === 0xa.U, Mux(is_hit, "b10000".U(5.W), "b00010".U(5.W)), "b01000".U(5.W)),
         (state_reg === "b00010".U(5.W)) -> Mux(io.r_valid, Mux(k === 3.U, "b00100".U(5.W), "b00010".U(5.W)), "b00010".U(5.W)),
@@ -44,16 +43,12 @@ class ysyx_25030077_IFU extends Module {
     val write_reg = RegInit("b001".U(3.W))
     write_reg := MuxCase("b001".U(3.W), Seq(
         (write_reg === "b001".U(3.W)) -> Mux(state_reg === "b00010".U(5.W) , "b010".U(3.W), "b001".U(3.W)),
-        (write_reg === "b010".U(3.W)) -> Mux(j === 3.U, "b100".U(3.W), "b010".U(3.W)),
+        (write_reg === "b010".U(3.W)) -> "b100".U(3.W),
         (write_reg === "b100".U(3.W)) -> Mux(q === 3.U, "b001".U(3.W), "b100".U(3.W)),
     ))
     k := MuxCase(k, Seq(
         (state_reg === "b00001".U(5.W)) -> 0.U,
         (state_reg === "b00010".U(5.W)) -> Mux(io.r_valid, Mux(k === 3.U, 0.U, k + 1.U), k)
-    ))
-    j := MuxCase(j, Seq(
-        (write_reg === "b001".U(3.W)) -> 0.U,
-        (write_reg === "b010".U(3.W)) -> Mux(j === 3.U, 0.U, j + 1.U)
     ))
     q := MuxCase(q, Seq(
         (state_reg === "b00001".U(5.W)) -> 0.U,
@@ -64,15 +59,9 @@ class ysyx_25030077_IFU extends Module {
     sdram_data(2) := Mux((state_reg === "b00010".U(5.W) && io.r_valid && k === 2.U), io.r_data, sdram_data(2))
     sdram_data(3) := Mux((state_reg === "b00010".U(5.W) && io.r_valid && k === 3.U), io.r_data, sdram_data(3))
 
-    sram_tag(Cat(index, "b00".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 0.U), tag, sram_tag(Cat(index, "b00".U(2.W))))
-    sram_tag(Cat(index, "b01".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 1.U), tag, sram_tag(Cat(index, "b01".U(2.W))))
-    sram_tag(Cat(index, "b10".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 2.U), tag, sram_tag(Cat(index, "b10".U(2.W))))
-    sram_tag(Cat(index, "b11".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 3.U), tag, sram_tag(Cat(index, "b11".U(2.W)))) 
+    sram_tag(index) := Mux(write_reg === "b010".U(3.W), tag, sram_tag(index))
 
-    sram_valid(Cat(index, "b00".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 0.U), "b1".U, sram_valid(Cat(index, "b00".U(2.W))))
-    sram_valid(Cat(index, "b01".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 1.U), "b1".U, sram_valid(Cat(index, "b01".U(2.W))))
-    sram_valid(Cat(index, "b10".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 2.U), "b1".U, sram_valid(Cat(index, "b10".U(2.W))))
-    sram_valid(Cat(index, "b11".U(2.W))) := Mux((write_reg === "b010".U(3.W) && j === 3.U), "b1".U, sram_valid(Cat(index, "b11".U(2.W)))) 
+    sram_valid(index) := Mux(write_reg === "b010".U(3.W), "b1".U, sram_valid(index))
 
     sram_inst(Cat(index, "b00".U(2.W))) := Mux((state_reg === "b00100".U(5.W) && q === 0.U), sdram_data(0), sram_inst(Cat(index, "b00".U(2.W))))
     sram_inst(Cat(index, "b01".U(2.W))) := Mux((state_reg === "b00100".U(5.W) && q === 1.U), sdram_data(1), sram_inst(Cat(index, "b01".U(2.W))))
