e7b2d516c148f8d84c51397d9f3ec13b857137e0
tracer-ysyx <tracer@ysyx.org>
2026-03-23 11:54:19 +0800

    > run NEMU ysyx_22040000 张三 Linux meowth 5.15.0-1026-intel-iotg #31-Ubuntu SMP Thu Feb 16 23:20:09 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:54:19 up  1:52,  1 user,  load average: 0.84, 0.67, 0.54

--- project/d_pipline_Soc/btracesim/btracesim.c
+++ project/d_pipline_Soc/btracesim/btracesim.c
@@ -9,6 +9,7 @@ typedef struct {
 } BTBEntry;
 
 BTBEntry table[TABLE_SIZE];
+uint8_t bht[TABLE_SIZE];  // 2-bit predictor
 
 int get_index(uint32_t pc) {
   return (pc >> 2) % TABLE_SIZE;
@@ -21,8 +22,10 @@ int main() {
     return 1;
   }
 
+  // 初始化
   for (int i = 0; i < TABLE_SIZE; i++) {
     table[i].valid = 0;
+    bht[i] = 2;  // 初始为 weak not taken (01)
   }
 
   int b_type;
@@ -32,7 +35,8 @@ int main() {
 
   int correct_btb = 0;
   int correct_nt  = 0;
-  int correct_at  = 0;   // 新策略
+  int correct_at  = 0;
+  int correct_2bit = 0;  // 新增
 
   while (fscanf(fp, "%d %x %x %x", &b_type, &pc_now, &pc_target, &pc_next) == 4) {
 
@@ -42,6 +46,10 @@ int main() {
     uint32_t pred_btb;
     uint32_t pred_nt;
     uint32_t pred_at;
+    uint32_t pred_2bit;
+
+    // ===== 实际是否跳转 =====
+    int actual_taken = (pc_next != pc_now + 4);
 
     // ===== 策略1：BTB + backward taken =====
     if (hit && b_type != 1) {
@@ -63,16 +71,41 @@ int main() {
       pred_at = pc_now + 4;
     }
 
+    // ===== 策略4：2-bit predictor =====
+    if (hit && b_type != 1) {
+      if (bht[idx] >= 2) {
+        pred_2bit = pc_target;  // predict taken
+      } else {
+        pred_2bit = pc_now + 4; // predict not taken
+      }
+    } else {
+      pred_2bit = pc_now + 4;
+    }
+
     // ===== 计分 =====
     if (pred_btb == pc_next) correct_btb++;
     if (pred_nt  == pc_next) correct_nt++;
     if (pred_at  == pc_next) correct_at++;
+    if (pred_2bit == pc_next) correct_2bit++;
 
     total++;
 
-    // ===== 更新 BTB =====
-    table[idx].tag = pc_now;
-    table[idx].valid = 1;
+    // ===== 更新 2-bit predictor =====
+    if(b_type != 1){
+      if(hit){
+        if (actual_taken) {
+          if (bht[idx] < 3) bht[idx]++;
+        } else {
+          if (bht[idx] > 0) bht[idx]--;
+        }
+      }
+      else{
+          // ===== 更新 BTB =====
+          table[idx].tag = pc_now;
+          table[idx].valid = 1;
+          bht[idx] = 2;
+      }
+    }
   }
 
   fclose(fp);
@@ -87,6 +120,9 @@ int main() {
 
   printf("\n[Always Taken (on hit)]\n");
   printf("Accuracy: %.2f%%\n", (double)correct_at / total * 100);
+
+  printf("\n[2-bit Predictor]\n");
+  printf("Accuracy: %.2f%%\n", (double)correct_2bit / total * 100);
 
   return 0;
 }
